eba19ae63972b897879783245d4f42fe9d1c216f
ynujeqax <stas.ynujeqax@gmail.com>
2026-01-09 15:01:09 +0200

    Reduce top padding for tabs to pt-14

--- frontend/src/pages/Profile/Subscription.tsx
+++ frontend/src/pages/Profile/Subscription.tsx
@@ -209,7 +209,7 @@ export default function Subscription() {
       />
 
       {/* Billing Period Toggle */}
-      <div className="px-4 pt-[68px] pb-3">
+      <div className="px-4 pt-14 pb-3">
         <Tabs
           tabs={[
             { id: 'monthly', label: 'Monthly' },
